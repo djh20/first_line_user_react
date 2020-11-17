d112726f433126ac8629791519bab0e2f36b77c2
LireEruel <jsh001505@gmail.com>
2020-11-17 15:06:57 +0900

    오류 수정

--- src/models/Post.js
+++ src/models/Post.js
@@ -1,8 +1,7 @@
 export default class Post{
-    constructor(post_id,title,num_lookup,text,like,num_reply,tag,writer,writing_date,editing_date,temperature,keyword){
+    constructor(post_id,title,text,like,num_reply,tag,writer,writing_date,editing_date,temperature,keyword){
         this.post_id = post_id
         this.title = title
-        this.num_lookup = num_lookup
         this.text = text
         this.like = like
         this.num_reply = num_reply
@@ -12,19 +11,6 @@ export default class Post{
         this.editing_date = editing_date
         this.temperature = temperature
         this.keyword = keyword
-    }
-
-    get_dic(){
-        return{
-            title : this.title,
-            like : this.like,
-            num_lookup : this.num_lookup,
-            num_reply : this.num_reply,
-            writing_date : this.writing_date,
-            editing_date : this.editing_date,
-            temperature : this.temperature,
-            keyword : this.keyword,
-        }
     }
 }
 
